d9ca0a9f487a6c255571eb204907825a3b2ef523
hao-zhang-05 <hao@mmtech.com.au>
2025-08-04 12:15:17 +1000

    added navigation

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Homepage from "./pages/Home";
 import ProductPage from "./pages/Products";
+import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
-  { path: '/', element: <Homepage /> },
-  { path: '/products', element: <ProductPage /> },
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { path: '/', element: <Homepage /> },
+      { path: '/products', element: <ProductPage /> },
+    ],
+  },
 ]);
 
 function App() {
